8314ebc61bc408f3ec33b6a4f613dbe58562a2c3
Ashish Debnath <ashishume@gmail.com>
2022-07-19 23:37:19 +0530

    feat: more hooks added

--- src/Routing/routes.tsx
+++ src/Routing/routes.tsx
@@ -1,14 +1,17 @@
 import { ReactElement } from "react";
 import App from "../App";
 import FolderComponent from "../components/FolderComponent";
-import ContextParent from "../containers/ContextParent";
 import Photo from "../containers/Photo";
 import { FolderData } from "../Data/FolderData";
 import Dashboard from "../e-commerce/components/dashboard";
 import Cart from "../e-commerce/containers/cart";
+import Input from "../hooks/customHooks/input";
 import UseCallbackHook from "../hooks/UseCallback/useCallback";
+import UseContext from "../hooks/UseContext";
 import Factorial from "../hooks/useMemo";
+import UseMemo2 from "../hooks/useMemo2";
 import StopWatch from "../hooks/useReducer";
+import UseRef from "../hooks/useRef";
 
 export const routes: {
   routeName: string;
@@ -31,6 +34,10 @@ export const routes: {
     component: <Factorial />,
   },
   {
+    routeName: "use-memo2",
+    component: <UseMemo2 />,
+  },
+  {
     routeName: "use-reducer",
     component: <StopWatch />,
   },
@@ -40,7 +47,7 @@ export const routes: {
   },
   {
     routeName: "context-api",
-    component: <ContextParent />,
+    component: <UseContext />,
   },
   {
     routeName: "store",
@@ -49,5 +56,13 @@ export const routes: {
   {
     routeName: "store/cart",
     component: <Cart />,
+  },
+  {
+    routeName: "use-ref",
+    component: <UseRef />,
+  },
+  {
+    routeName: "custom-hooks",
+    component: <Input />,
   },
 ];
