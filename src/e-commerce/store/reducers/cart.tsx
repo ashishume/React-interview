77bd965bf6afa57a906f24a6b23c4d10a401145d
Ashish Debnath <ashishume@gmail.com>
2022-07-19 21:04:56 +0530

    feat: redux reducers changed

--- src/e-commerce/store/reducers/cart.tsx
+++ src/e-commerce/store/reducers/cart.tsx
@@ -15,7 +15,12 @@ const cartReducer = (
       /**
        *  action.payload gets the index of the item
        */
-      cartItems[action.payload].isAdded = true;
+      const newCartItem = {
+        ...cartItems[action.payload],
+        isAdded: true,
+      };
+      cartItems.slice(action.payload, 1);
+      cartItems[action.payload] = newCartItem;
       return {
         ...state,
         cart: cartItems,
@@ -24,9 +29,18 @@ const cartReducer = (
       /** already have the index and just change the boolean to false to remove from cart
        * action.payload gets the index of the item
        */
-      cartItems[action.payload].isAdded = false;
-      if (cartItems[action.payload]?.quantity)
-        cartItems[action.payload].quantity = 0;
+      let newCartItemRemoval = {
+        ...cartItems[action.payload],
+        isAdded: false,
+      };
+      if (cartItems[action.payload]?.quantity) {
+        newCartItemRemoval = {
+          ...newCartItemRemoval,
+          quantity: 0,
+        };
+      }
+      cartItems.slice(action.payload, 1);
+      cartItems[action.payload] = newCartItemRemoval;
       return {
         ...state,
         cart: cartItems,
@@ -38,22 +52,28 @@ const cartReducer = (
        * make it 2(as adding to cart means 1 quantity added)
        * else just add +1 to cart
        * */
-      cartItems[action.payload].quantity =
-        !!!incIndexItem || incIndexItem <= 1 ? 2 : incIndexItem + 1;
+      let incQuantityItem = {
+        ...cartItems[action.payload],
+        quantity: !!!incIndexItem || incIndexItem <= 1 ? 2 : incIndexItem + 1,
+      };
+      cartItems.slice(action.payload, 1);
+      cartItems[action.payload] = incQuantityItem;
       return {
         ...state,
         cart: cartItems,
       };
     case ActionTypes.DECREASE_QUANTITY:
       /** change the quantity to decrease the cart value */
-
       const decIndexItem = cartItems[action.payload]?.quantity;
-
       /** if quantity key property doesnt exist or less than equal to 1 then,
        * make it 1, as item cannot have less than 1, (0 means removed from cart)
        * */
-      cartItems[action.payload].quantity =
-        !!!decIndexItem || decIndexItem <= 1 ? 1 : decIndexItem - 1;
+      let decQuantityItem = {
+        ...cartItems[action.payload],
+        quantity: !!!decIndexItem || decIndexItem <= 1 ? 1 : decIndexItem - 1,
+      };
+      cartItems.slice(action.payload, 1);
+      cartItems[action.payload] = decQuantityItem;
       return {
         ...state,
         cart: cartItems,
